93618402956ef3f5c02da717505f60b8258638ae
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2024-03-29 00:27:15 +0700

    Delete docs/hollowengine/.resources/chat directory

--- docs/hollowengine/.resources/chat/chatBlock.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import type from './chatType.module.css';
-
-const Chat = ({ children }) => {
-  return <div className={type.chat}>{children}</div>;
-};
-
-export default Chat;
