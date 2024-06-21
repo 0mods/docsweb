a8891d4881c7de905f05cdc633fe2f00bb586a77
_BENDY659_ [RU] <109421782+BENDY659@users.noreply.github.com>
2024-06-22 06:51:32 +0900

    Rebool attempt 2. ALPHA

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import type from './chatType.module.css';
+//import type from './phone.css'
 
 const Chat = ({ children }) => {
   return <div className={type.chat}>{children}</div>;
