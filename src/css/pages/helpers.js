40d946ee571ab0d6ea1420e6fb8af8ffeddebc2e
bendy659 <denisglazyrin13@gmail.com>
2024-09-05 20:54:13 +0900

    Да

--- src/css/pages/helpers.js
+++ src/css/pages/helpers.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Helper = ({ name, children }) => {
-  const article = document.querySelector('article');
-  const newElement = document.createElement('div');
-  newElement.textContent = 'Новый элемент';
-  article.appendChild(newElement;);
-}
+const Helper = ({ children }) => {
+  useEffect(() => {
+    const article = document.querySelector('article');
+    const targetElement = document.querySelector('.theme-doc-markdown.markdown');
+    if (article && targetElement) {
+      const newElement = document.createElement('div');
+      newElement.className = 'helper';
+      newElement.textContent = children;
+      article.insertBefore(newElement, targetElement);
+    }
+  }, [children]);
+
+  return null;
+};
 
 export default Helper;
