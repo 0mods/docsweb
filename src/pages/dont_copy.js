9fd7232b0627713b3fe03e5d442db727e2f83aa8
bendy659 <denisglazyrin13@gmail.com>
2024-09-26 16:47:06 +0900

    ths оно?

--- src/pages/dont_copy.js
+++ src/pages/dont_copy.js
@@ -1,15 +1,23 @@
-document.addEventListener('copy', function(event) {
-event.preventDefault();
-});
-
-document.addEventListener('contextmenu', function(event) {
-event.preventDefault();
-});
-
-document.addEventListener('dragstart', function(event) {
-event.preventDefault();
-});
+import React, { useEffect } from 'react';
 
 /*
  * Нефиг копировать документацию >:(
 */
+
+const DontCopyFunc = () => {
+  useEffect(() => {
+      document.addEventListener('copy', function (event) {
+          event.preventDefault();
+      });
+
+      document.addEventListener('contextmenu', function (event) {
+          event.preventDefault();
+      });
+
+      document.addEventListener('dragstart', function (event) {
+          event.preventDefault();
+      });
+  })
+}
+
+export default DontCopyFunc;
