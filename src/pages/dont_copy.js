e92e4b7c40a166125f0f8badfd0e47dd47444f3e
bendy659 <denisglazyrin13@gmail.com>
2024-09-26 16:52:28 +0900

    Еее оно заработало, но у меня дс сдох :bruh:

--- src/pages/dont_copy.js
+++ src/pages/dont_copy.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 
-/*
+/**
  * Нефиг копировать документацию >:(
+ * Обычная защита от копирования текста, кода и картинок.
+ *
+ * @author \_BENDY659\_ & HalvaBot
 */
-
 const DontCopyFunc = () => {
   useEffect(() => {
       document.addEventListener('copy', function (event) {
