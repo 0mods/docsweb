2a4694c392a8ebf0454a4438fa9c23f5b05d2c66
_BENDY659_ <denisglazyrin13@gmail.com>
2024-12-29 17:18:47 +0900

    ура

--- src/components/Chat/chat.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type from './chatType.module.css';
-//import type from './phone.css'
-
-const Chat = ({ children }) => {
-  return <div className={type.chat}>{children}</div>;
-};
-
-export default Chat;
