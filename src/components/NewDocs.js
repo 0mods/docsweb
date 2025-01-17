2abab5d9c007f52687dec20b8c29705a6db496ba
_BENDY659_ <denisglazyrin13@gmail.com>
2025-01-17 22:34:59 +0900

    Возможно исправлена ошибка при Перенаправлении

--- src/components/NewDocs.js
+++ src/components/NewDocs.js
@@ -3,8 +3,9 @@ import { useEffect } from 'react';
 const ToBeta = ({ url }) => {
   useEffect(() => {
     const link = 'https://beta.0mods.team/ru-RU/docs/Legacy/hollowengine/'+url;
-     alert('Читай актуальны доки, а не эту парашу :)')
-    window.location.href = link
+    alert('Читай актуальны доки, а не эту парашу :)')
+    window.history.pushState(null, null, link);
+    window.location.href = link;
     console.log('Open beta docs from link: '+link)
   }, [url])
   return null;
